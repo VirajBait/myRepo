8371fd2cd54d21f9b89057675a5b600cae3d732a
viraj <viraj@DESKTOP-JC7U56L>
2024-01-08 16:33:40 +0530

    First Commit

---
+++ simpleApp/src/test/java/com/simpleProject/demo/SimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleProject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
